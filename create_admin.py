5506c64dd13cae660b0890c75de31ac100a41520
ntf6drtx6d-blip <dkucheruk@gmail.com>
2026-03-23 10:09:03 +0100

    Update create_admin.py

--- create_admin.py
+++ create_admin.py
@@ -1,1 +1,16 @@
+from core.db import init_db, create_user
+from core.auth import hash_password
 
+# 1. створюємо базу (якщо ще нема)
+init_db()
+
+# 2. створюємо користувача
+create_user(
+    email="admin@sala.com",
+    password_hash=hash_password("1234"),
+    role="admin",
+    full_name="Admin",
+    organization="SALA"
+)
+
+print("Admin user created.")

d3740e6dfb8b30db4c7d24f765eb1b11c10e2761
ntf6drtx6d-blip <dkucheruk@gmail.com>
2026-03-23 14:30:37 +0100

    Update theme.py

--- report/theme.py
+++ report/theme.py
@@ -1,1 +1,34 @@
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.colors import HexColor, white
 
+PAGE_W, PAGE_H = A4
+
+SALA_LOGO = "sala_logo.png"
+EU_LOGO = "logo_en.gif"
+
+NAVY = HexColor("#0F172A")
+TEXT = HexColor("#344054")
+MUTED = HexColor("#667085")
+LINE = HexColor("#DDE3EA")
+SOFT_BG = HexColor("#F8FAFC")
+
+RED = HexColor("#B42318")
+RED_BG = HexColor("#FEF3F2")
+RED_BORDER = HexColor("#F7C7C1")
+
+BLUE = HexColor("#1F4FBF")
+BLUE_SOFT = HexColor("#EEF4FF")
+BLUE_BORDER = HexColor("#D6E4FF")
+
+GREEN = HexColor("#067647")
+GREEN_BG = HexColor("#ECFDF3")
+GREEN_BORDER = HexColor("#ABEFC6")
+
+GOLD = HexColor("#B54708")
+GOLD_BG = HexColor("#FFF7DB")
+GOLD_BORDER = HexColor("#F5C451")
+
+WHITE = white
+
+MARGIN = 46
+TOP_RULE_Y = PAGE_H - 40

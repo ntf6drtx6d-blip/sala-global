fd1afbfc93ffe2467652954dcd6e4bdf2d05d250
ntf6drtx6d-blip <dkucheruk@gmail.com>
2026-03-24 10:05:12 +0100

    Update setup.py

--- ui/setup.py
+++ ui/setup.py
@@ -154,7 +154,7 @@ def longest_night_hours(lat_deg: float) -> float:
 
 def _device_label(device_id):
     d = DEVICES[device_id]
-    return f"{d['code']}"
+    return d["name"]
 
 
 def _engine_summary(device_id, engine_key, battery_mode):
